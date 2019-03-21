30cc989119aa27f7cd3b0d98e37357672d17d8f2
Soumya <soumya.poolla@gmail.com>
2019-03-20 17:15:04 -0700

    finished compose email

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ComposeEmailComponent} from './compose-email/compose-email.component';
+import {ListEmailComponent} from './list-email/list-email.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create', component:ComposeEmailComponent},
+  {path: 'list', component:ListEmailComponent},
+  {path: '',redirectTo:'/list',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
